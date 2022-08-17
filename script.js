b74c4841f8784bac9d8e89ebda1a920e6d5173ba
farah328 <109863407+farah328@users.noreply.github.com>
2022-08-17 20:07:09 +0300

    some change

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 
+function BMI (w,h){
+    let result = w / (h*h);
+    return result
+}
+
+console.log(BMI(10,10))
+
+function status (bmi) {
+if( bmi< 18.5){
+    return "نقص بالوزن"
+} else if (bmi >=18.5 && bmi < 25){
+    return "وزنك صحي "
+}else if (bmi >=25){
+    return "لديك زياده بالوزن"
+}
+
+}
+
+
+function calculate () {
+
+    let weight =document.getElementById("weight").value
+    let height =document.getElementById("height").value
+
+    let bmi = BMI( weight,height)
+    let desc =  status(bmi)
+
+    let div =document.getElementById("result")
+    div.innerHTML = bmi + " == " + desc
+}
